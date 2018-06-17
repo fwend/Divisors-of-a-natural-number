5897e967e589efd45debef00c06bc8868b664d26
Jos <josvanuden@gmail.com>
2018-06-17 12:26:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Divisors-of-a-natural-number
---
+++ main.js
@@ -0,0 +1,20 @@
+function getDivisors(n) {
+    if (n === 1)
+        return [n];
+
+    const result = [1, n];
+    const limit = Math.sqrt(n) >> 0;
+
+    for (let p = 2; p <= limit; p++) {
+
+        if (n % p === 0) {
+            result.push(p);
+
+            const q = n / p;
+            if (q > p)
+                result.push(q);
+        }
+    }
+
+    return result.sort((a, b) => a - b);
+}
