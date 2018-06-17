0b169a96cab56546d374cfd394adc1b361c5c108
Jos <josvanuden@gmail.com>
2018-06-17 13:58:56 +0200

    added proper divisors

--- main.js
+++ main.js
@@ -18,3 +18,20 @@ function getDivisors(n) {
 
     return result.sort((a, b) => a - b);
 }
+
+function properDivisors(n) {
+    const result = [1];
+    const limit = Math.sqrt(n) >> 0;
+
+    for (let p = 2; p <= limit; p++) {
+
+        if (n % p === 0) {
+            result.push(p);
+
+            const q = n / p;
+            if (q > p)
+                result.push(q);
+        }
+    }
+    return result;
+}
